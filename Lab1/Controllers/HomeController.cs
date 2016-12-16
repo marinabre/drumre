f9f23618ea05266c5444bde9041d969f88d8db56
marinabre <marina.brebric@fer.hr>
2016-12-16 12:02:29 +0100

    maknuta baza iz gita (i dodano u gittignore), dodan kod za povlačenje filmova

--- Lab1/Controllers/HomeController.cs
+++ Lab1/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Lab1.App_Start;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
 
 namespace Lab1.Controllers
 {
@@ -21,11 +26,88 @@ namespace Lab1.Controllers
             return View();
         }
 
+
+        // public async System.Threading.Tasks.Task<ActionResult> Contact()
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            //za pozivanje dohvata iz imdba
+            //return View(await IMDB2());
             return View();
+
         }
+        public async System.Threading.Tasks.Task<List<Movie>> IMDB2()
+        {
+            var IMDB = new TMDbClient("2c54085e8a7f520650d65cb78a48555a");
+            //var results2 = await IMDB.GetMovieAsync(47964, MovieMethods.Credits | MovieMethods.Videos);
+            List<TMDbLib.Objects.Movies.Movie> rez = new List<Movie>();
+            // 5998528
+            //54001 zadnji id u bazi
+            for (int j = 54001; j <= 100000; j += 1000)
+            {
+                for (int i = j; i <= j + 1000; i++)
+                {
+                    Movie pom = new Movie();
+                    try
+                    {
+                        pom = await IMDB.GetMovieAsync(i, MovieMethods.Credits | MovieMethods.Videos);
+
+                    }
+                    catch
+                    {
+                        spremiFilmove(rez);
+                        rez = null;
+                    }
+
+                    if (pom.ImdbId != null)
+                    {
+                        rez.Add(pom);
+                    }
+                }
+                if (rez != null)
+                {
+                    spremiFilmove(rez);
+                }
+
+            }
+            return rez;
+
+        }
+        public async void spremiFilmove(List<Movie> newObjects)
+        {
+            var db = MongoInstance.GetDatabase;
+            var collection = db.GetCollection<Movie>("movies");
+            await collection.InsertManyAsync(newObjects);
+
+            //ima više smisla kada se unose filmovi koji se pretražuju preko abecede
+            //da ne pizdi zbog duplića
+            // var models = new WriteModel<BsonDocument>[newObjects.Count];
+            //// use ReplaceOneModel with property IsUpsert set to true to upsert whole documents
+            //for (var i = 0; i < newObjects.Count; i++)
+            //{
+            //    var bsonDoc = newObjects[i].ToBsonDocument();
+            //    models[i] = new ReplaceOneModel<BsonDocument>(new BsonDocument("_id", newObjects[i].Id), bsonDoc) { IsUpsert = true };
+            //};
+            //await collection.BulkWriteAsync(models);
+        }
+
+        ////thetvdb.com API KEY: BDA5FCAB219B7E8E
+        //public List<BazaPodataka.TVShow> thetvdb()
+        //{
+        //    var tvdb = new TVDB("BDA5FCAB219B7E8E");
+        //    var results = tvdb.Search(name);
+        //    var shows = new List<BazaPodataka.TVShow>();
+        //    tvdb.Search("", Int32.MaxValue);
+
+        //    foreach (TVDBSharp.Models.Show item in results)
+        //    {
+        //        shows.Add(new BazaPodataka.TVShow(item.ImdbId, item.Name, item.Rating ?? 0, item.Description, item.Actors));
+        //    }
+
+        //    baza.unesiEmisije(shows);
+        //    return shows;
+        //}
+
+
+
     }
 }
---
+++ komanda.cmd
@@ -0,0 +1,1 @@
+mongod --dbpath=\Users\Ivyclover\Documents\GitHub\drumre\baza
