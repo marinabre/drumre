847fae9966de9c8084d05ecfb1d80a574cdcc643
Ana <ana.klopovic@fer.hr>
2016-12-21 23:42:15 +0100

    začeće Recommendera

---
+++ BLL/MovieSimilarity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class MovieSimilarity
+    {
+        Movie movieA;
+        Movie movieB;
+        decimal actorSimilarity;
+        decimal directorSimilarity;
+        decimal genreSimilarity;
+    }
+}
---
+++ BLL/Recommender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Movies;
+
+namespace BLL
+{
+    class Recommender
+    {
+        public decimal calculateActorSimilarity (Movie movieA, Movie movieB)
+        {
+            IList<Cast> castA = movieA.Credits.Cast;
+            IList<Cast> castB = movieB.Credits.Cast;
+
+            decimal intersection = castA.Select(a => a.Name).Intersect(castB.Select(b => b.Name)).Count();
+            decimal joined = castA.Count() + castB.Count();
+
+            return 2 * intersection / joined;
+        }
+
+        public decimal calculateDirectorSimilarity(Movie movieA, Movie movieB)
+        {
+            IList<Crew> directorsA = movieA.Credits.Crew.Where(x => x.Job == "Director").ToList();
+            IList<Crew> directorsB = movieB.Credits.Crew.Where(x => x.Job == "Director").ToList();
+
+            decimal intersection = directorsA.Select(a => a.Name).Intersect(directorsB.Select(b => b.Name)).Count();
+            decimal joined = directorsA.Count() + directorsB.Count();
+
+            return 2 * intersection / joined;
+        }
+
+        public decimal calculateGenreSimilarity(Movie movieA, Movie movieB)
+        {
+            IList<Genre> genresA = movieA.Genres;
+            IList<Genre> genresB = movieB.Genres;
+
+            decimal intersection = genresA.Intersect(genresB).Count();
+            decimal joined = genresA.Count() + genresB.Count();
+
+            return 2 * intersection / joined;
+        }
+
+        //public MovieSimilarity getSimilarity (Movie movieA, Movie movieB)
+        //{
+        //    var db = MongoInstance.GetDatabase;
+        //    var similar = db.GetCollection<MovieSimilarity>("Similar");
+        //    similar.FindAsync
+        //}
+
+    }
+}
