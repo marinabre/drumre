f0f7ae39b0b18b1d471a4f793c95a92f653a523d
Ana <ana.klopovic@fer.hr>
2016-12-22 12:35:51 +0100

    dodan projekt za testiranje

---
+++ Testovi/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testovi
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
