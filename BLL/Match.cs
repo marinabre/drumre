277f7343fa602102cdb9cc5795bdaf2dd9153fdd
Ana <ana.klopovic@fer.hr>
2016-12-23 00:55:40 +0100

    Do you know who your friends really are?

--- BLL/Match.cs
+++ BLL/Match.cs
@@ -8,8 +8,30 @@ namespace BLL
 {
     public class Match
     {
-        bool sameGender;
-        int yearDiff;
-        //IList<Person>
+        public Person personA;
+        public Person personB;
+        public bool sameGender;
+        public int ageDiff;
+        public IList<string> commonFriends;
+        public IList<FBMovie> commonMovies;
+
+        public Match()
+        {
+            sameGender = false;
+            commonFriends = new List<string>();
+            commonMovies = new List<FBMovie>();
+        }
+
+        public Match (Person A, Person B)
+        {
+            personA = A;
+            personB = B;
+            if (A.Gender == B.Gender) this.sameGender = true;
+            this.ageDiff = Math.Abs(A.Birthday.Year - B.Birthday.Year);
+            this.commonFriends = A.Friends.Intersect(B.Friends).ToList();
+            IList<FBMovie> moviesA = A.LikedMovies.Union(A.Watches).ToList();
+            IList<FBMovie> moviesB = B.LikedMovies.Union(B.Watches).ToList();
+            this.commonMovies = moviesA.Intersect(moviesB).ToList();
+        }
     }
 }
