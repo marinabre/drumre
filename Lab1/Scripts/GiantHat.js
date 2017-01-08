bf33c3af3b5742ff7707af9a5c7aa3ac80ae74f4
Marina Brebric <marina.brebric@fer.hr>
2017-01-08 18:33:57 +0100

    premješten checkbox i dodana logika da se označi/odznači zajedno kako se i filteri collapsaju

--- Lab1/Scripts/GiantHat.js
+++ Lab1/Scripts/GiantHat.js
@@ -1,34 +1,35 @@
-﻿    $(document).ready(function() {
-        $(".ProfileDetails").addClass("hidden");
-    });
-     $(document).ready(function() {
-        $(".FriendsDetails").addClass("hidden");
-    });
-     $(document).ready(function() {
-        $(".CommunityDetails").addClass("hidden");
-    });
-
+﻿$(document).ready(function() {
+	$("#Profile").prop("checked", true);
+	$("#Friends").prop("checked", true);
+});
+    
     $(document).on("click",".Profile",function() {
         if ($(".ProfileDetails").hasClass("hidden")) {
+            $("#Profile").prop("checked", true);
             $(".ProfileDetails").removeClass("hidden");
         }
         else {
+            $("#Profile").prop("checked", false);
             $(".ProfileDetails").addClass("hidden");
         }        
     });
     $(document).on("click",".Friends",function() {
         if ($(".FriendsDetails").hasClass("hidden")) {
+            $("#Friends").prop("checked", true);
             $(".FriendsDetails").removeClass("hidden");
         }
         else {
+         $("#Friends").prop("checked", false);
             $(".FriendsDetails").addClass("hidden");
         }        
     });
     $(document).on("click",".Community",function() {
         if ($(".CommunityDetails").hasClass("hidden")) {
+        $("#Community").prop("checked", true);
             $(".CommunityDetails").removeClass("hidden");
         }
         else {
+        $("#Community").prop("checked", false);
             $(".CommunityDetails").addClass("hidden");
-        }        
-    });
+        }    });
+  
